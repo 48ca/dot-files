abbc0d68a47be4a2857180f856415573906d3fe1
James Houghton <jamesthoughton@gmail.com>
2017-09-29 16:07:56 -0400

    Ampersand fix

--- .config/polybar/spotify.sh
+++ .config/polybar/spotify.sh
@@ -12,11 +12,11 @@ main() {
   meta=$(dbus-send --print-reply --dest=${domain}.spotify \
     /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:${domain}.Player string:Metadata)
 
-  artist=$(echo "$meta" | sed -nr '/xesam:artist"/,+2s/^ +string "(.*)"$/\1/p' | tail -1 | sed 's/\//\\\//g')
-  album=$(echo "$meta" | sed -nr '/xesam:album"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed 's/\//\\\//g')
-  title=$(echo "$meta" | sed -nr '/xesam:title"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed 's/\//\\\//g')
+  artist=$(echo "$meta" | sed -nr '/xesam:artist"/,+2s/^ +string "(.*)"$/\1/p' | tail -1 | sed 's/\//\\\//g;s/\&/\\&/g')
+  album=$(echo "$meta" | sed -nr '/xesam:album"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed 's/\//\\\//g;s/\&/\\&/g')
+  title=$(echo "$meta" | sed -nr '/xesam:title"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed 's/\//\\\//g;s/\&/\\&/g')
 
-  echo "${*:-%artist% - %title%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g;s/%album%/$album/g"i | sed 's/&/\\&/g'
+  echo "${*:-%artist% - %title%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g;s/%album%/$album/g"i
 }
 
 main "$@"
